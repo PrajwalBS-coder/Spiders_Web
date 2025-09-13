87410b3f3706d63385004c660153fbb5fa123461
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-09-13 12:48:28 +0530

    refactor: optimize vowel/consonant frequency solution using Counter
    Replace inefficient nested loop approach with Counter-based solution for finding most frequent vowel and consonant characters, improving time complexity from O(n²) to O(n)
    
    Runtime 0ms
    Beats 100.00%

--- python/Leet Code/3541. Find Most Frequent Vowel and Consonant.py
+++ python/Leet Code/3541. Find Most Frequent Vowel and Consonant.py
@@ -1,13 +1,23 @@
+# s = "pps"
+# v='aeiou'
+# c='qwrtypsdfghjklzxcvbnm'
+# vc=0
+# cc=0
+# for i in s:
+#     if i in v:
+#         if vc<s.count(i):
+#             vc=s.count(i)
+#     else:
+#         if cc<s.count(i):
+#             cc=s.count(i)
+# print(cc+vc)
+
+
+from collections import Counter
 s = "pps"
 v='aeiou'
-c='qwrtypsdfghjklzxcvbnm'
-vc=0
-cc=0
-for i in s:
-    if i in v:
-        if vc<s.count(i):
-            vc=s.count(i)
-    else:
-        if cc<s.count(i):
-            cc=s.count(i)
+mp = Counter(s)#mp={'p':2,'s':1}
+print(mp)
+vc = max((mp[ch] for ch in mp if ch in v), default=0)
+cc = max((mp[ch] for ch in mp if ch not in v), default=0)
 print(cc+vc)
