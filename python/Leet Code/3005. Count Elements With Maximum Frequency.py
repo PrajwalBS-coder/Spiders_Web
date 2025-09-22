adfa6cbd51717b5d3e2b1610f9746da248116bc2
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-09-22 10:23:31 +0530

    Runtime 0ms Beats 100.00%

--- python/Leet Code/3005. Count Elements With Maximum Frequency.py
+++ python/Leet Code/3005. Count Elements With Maximum Frequency.py
@@ -1,10 +1,31 @@
-nums = [1,2,3,4,5]
-maxc=1
-c=0
-for i in nums:
-    if(nums.count(i)>maxc):
-        maxc=nums.count(i)
-for i in nums:
-    if(nums.count(i)==maxc):
-        c+=1
-print(c)
+# nums = [1,2,3,4,5]
+# maxc=1
+# c=0
+# for i in nums:
+#     if(nums.count(i)>maxc):
+#         maxc=nums.count(i)
+# for i in nums:
+#     if(nums.count(i)==maxc):
+#         c+=1
+# print(c)
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: list[int]) -> int:
+        freq = [0] * 101#Generating Empty List of size 101
+        # print(freq,len(freq))
+        max = 0
+        res = 0
+        for n in nums:
+            freq[n] += 1#incrementing the frequency
+            f = freq[n]
+            # print(f,freq)
+            if f > max:#
+                max = f
+                res = f
+            elif f == max:
+                res += f
+        return res
+
+nums =  [1,2,2,3,1,4]
+print(Solution().maxFrequencyElements(nums))
