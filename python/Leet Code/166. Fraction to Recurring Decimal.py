3248a21f47f77294fd4abaad9e91ee27c02f3106
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-09-24 09:47:17 +0530

    20 / 41 testcases passed

--- python/Leet Code/166. Fraction to Recurring Decimal.py
+++ python/Leet Code/166. Fraction to Recurring Decimal.py
@@ -1,3 +1,9 @@
-numerator = 1
-denominator = 2
-print(str(int(numerator/denominator)))
+numerator = 4
+denominator = 333
+l=(str(numerator/denominator).split(".")[1])
+# print(l)
+if l=="0":
+    print(str(int(numerator/denominator)))
+elif len(l)>2:
+    l="".join(dict.fromkeys(l))
+    print(str(int(numerator/denominator))+"."+"("+l+")")
