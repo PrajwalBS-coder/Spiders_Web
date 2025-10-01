3cf5713c9037684cc339165916adfd240c12349f
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-10-01 09:56:19 +0530

    refactor: replace iterative solution with mathematical formula
    Replaced the while loop implementation with a direct mathematical formula
    for calculating total consumable water bottles. The new approach uses
    `numBottles+(numBottles-1)//(numExchange-1)` which eliminates the need
    for iteration and provides O(1) time complexity instead of O(n).
    
    Also updated test case (numExchange: 7 -> 4) for validation.

--- python/Leet Code/1518. Water Bottles.py
+++ python/Leet Code/1518. Water Bottles.py
@@ -1,8 +1,10 @@
 numBottles = 15
-numExchange = 7
+numExchange = 4
 consumed_bottles = 0
-while numBottles >= numExchange:
-    consumed_bottles += numExchange
-    numBottles -= numExchange
-    numBottles += 1
-print(numBottles+consumed_bottles)
+# while numBottles >= numExchange:
+#     consumed_bottles += numExchange
+#     numBottles -= numExchange
+#     numBottles += 1
+# print(numBottles+consumed_bottles)
+print(numBottles+(numBottles-1)//(numExchange-1))#Here the fist op is (numBottles-1)//(numExchange-1) which will  return the value of the add with numBottles
+
