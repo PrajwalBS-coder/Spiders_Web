6d23c6301253ff8fb7ef821b84861ac8f2bfc34b
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-11-25 09:43:29 +0530

    Wrong Answer 28 / 70 testcases passed

---
+++ python/Leet Code/1015. Smallest Integer Divisible by K.py
@@ -0,0 +1,14 @@
+k = 5
+binary_str="0"
+for i in range(k-1):
+    binary_str+="1"
+# print(int(binary_str,2))
+print(binary_str)
+integer_num=int(binary_str,2)
+print(integer_num)
+if integer_num%k==0:
+    print(integer_num)
+    print(k)
+# print(int(binary_str,2)%k)
+else:
+    print(-1)
