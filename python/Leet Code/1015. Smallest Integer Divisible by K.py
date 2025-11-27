4d69751967a00eafdc3d8f682b6d93d327b56107
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-11-27 08:33:12 +0530

    1015 completd

--- python/Leet Code/1015. Smallest Integer Divisible by K.py
+++ python/Leet Code/1015. Smallest Integer Divisible by K.py
@@ -1,14 +1,24 @@
 k = 5
-binary_str="0"
-for i in range(k-1):
-    binary_str+="1"
-# print(int(binary_str,2))
-print(binary_str)
-integer_num=int(binary_str,2)
-print(integer_num)
-if integer_num%k==0:
-    print(integer_num)
-    print(k)
-# print(int(binary_str,2)%k)
-else:
-    print(-1)
+# binary_str="0"
+# for i in range(k-1):
+#     binary_str+="1"
+# # print(int(binary_str,2))
+# print(binary_str)
+# integer_num=int(binary_str,2)
+# print(integer_num)
+# if integer_num%k==0:
+#     print(integer_num)
+#     print(k)
+# # print(int(binary_str,2)%k)
+# else:
+#     print(-1)
+
+r =0 
+for i in range(k):
+    r= (r * 10)+1 
+    print(r)
+    r = r%k
+    print(r)
+    if r==0:
+        print (i+1)
+print (-1)
