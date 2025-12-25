ed79f96cd93cd2a64d673a593c3102aeb0c7f078
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-12-25 13:04:08 +0530

    Accepted 674 / 674 testcases passed

--- python/Leet Code/3075. Maximize Happiness of Selected Children.py
+++ python/Leet Code/3075. Maximize Happiness of Selected Children.py
@@ -1,10 +1,18 @@
-happiness = [2,98,45]
-k = 1
-happiness.sort()
-total_happiness =[]
+happiness = [12,1,42]
+k = 3
+# for i in range(k):
+#     if happiness[-1]<=0:
+#         continue
+#     total_happiness.append(happiness[-1])
+#     happiness.pop()
+#     happiness=list(map(lambda x: x - 1, happiness))
+# print((total_happiness))
+# print(sum(total_happiness))
+
+happiness.sort(reverse=True)
+total_happiness_sum = 0
+turns = 0
 for i in range(k):
-    total_happiness.append(happiness[-1])
-    happiness.pop()
-    happiness=list(map(lambda x: x - 1, happiness))
-print((total_happiness))
-print(sum(total_happiness))
+    total_happiness_sum += max(happiness[i] - turns, 0)
+    turns += 1
+print( total_happiness_sum)
